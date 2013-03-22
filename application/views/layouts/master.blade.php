88a2b87582ab8a0ff5700d2c78496931d3a821e1
pollylopper <pklein230@gmail.com>
2013-03-21 21:26:43 -0600

    col-1, col-2, col-3 layout

--- application/views/layouts/master.blade.php
+++ application/views/layouts/master.blade.php
@@ -2,7 +2,22 @@
 <html lang="en">
     @include('layouts.header')
 
+    <div class="lt col-1">
+        <ul>
+    <li><a href="#">Muse</a></li>
+    <li><a href="#">Mars Volta</a></li>
+    <li><a href="#">30 Seconds to Stars</a></li>
+    <li><a href="#">Tool</a></li>
+        </ul>
+    </div>
+
+    <div class="lt col-2">
         @yield('content')
+    </div>
+
+    <div class="rt col-3">
+
+    </div>
 
     @include('layouts.footer')
 </html>
