7048e29be9143623a5c5af0026703911ed577e9d
Randen <rkelly@rabbleandrouser.com>
2014-03-27 20:47:20 -0600

    updates to message handling; master layout formatting

--- application/views/things/add.blade.php
+++ application/views/things/add.blade.php
@@ -5,14 +5,10 @@
     <div class="form">
         <h1>Add a thing</h1>
 
-        @include('success_handler')
-
         {{ Form::label('thing_name', 'Thing') }}
         <div class="msg"></div>
 
         {{ Form::text('thing_name', Input::old('thing_name'), array('autocomplete' => 'off')) }}
-
-        @include('errors_handler')
 
         {{ Form::submit('add', array('class' => 'button')) }}
     </div>
